e5b2d01353324c0b032b970cfd8d747717513760
Juan Miguel Alvarez Tosco <juanmi.alvarez@gmail.com>
2024-10-27 09:57:47 +0000

    03_ticket/setters

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -11,22 +11,9 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
-
+        Ticket::check_title(&title);
+        Ticket::check_description(&description);
+        Ticket::check_status(&status);
         Ticket {
             title,
             description,
@@ -44,6 +31,45 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    fn check_title(title: &String) {
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+    }
+
+    pub fn set_title(&mut self, new_title: String) {
+        Ticket::check_title(&new_title);
+        self.title = new_title
+    }
+
+    fn check_description(description: &String) {
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+    }
+
+    pub fn set_description(&mut self, new_description: String) {
+        Ticket::check_description(&new_description);
+        self.description = new_description
+    }
+
+    fn check_status(status: &String) {
+        if status != "To-Do" && status != "In Progress" && status != "Done" {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+    }
+
+    pub fn set_status(&mut self, new_status: String) {
+        Ticket::check_status(&new_status);
+        self.status = new_status
     }
 }
 
