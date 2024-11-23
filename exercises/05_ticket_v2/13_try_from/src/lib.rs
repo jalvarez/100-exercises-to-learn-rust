a1c4838baf981491de2f12eeb543dba7544df076
Juan Miguel Alvarez Tosco <juanmi.alvarez@gmail.com>
2024-11-23 11:57:19 +0000

    05_ticket/try_from

--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -8,6 +8,35 @@ enum Status {
     Done,
 }
 
+#[derive(thiserror::Error, Debug)]
+#[error("{invalid_status} is an invalid status")]
+struct ParseStatusError {
+    invalid_status: String,
+}
+
+impl TryFrom<String> for Status {
+    type Error = ParseStatusError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Self::Done),
+            _ => Err(ParseStatusError {
+                invalid_status: value,
+            }),
+        }
+    }
+}
+
+impl TryFrom<&str> for Status {
+    type Error = ParseStatusError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Status::try_from(value.to_string())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
